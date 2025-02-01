cd38b1a3e118c0d64cec43c6139948f8f32ca7a9
Kasper Anon <kasperthornsten@gmail.com>
2025-02-01 18:01:47 +0100

    Refactored restless api to work with new JSON endpoint configuration system

--- smart-fridge/src/restless/app.py
+++ smart-fridge/src/restless/app.py
@@ -1,9 +1,13 @@
 import os
 import json
 
+import importlib
+
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
+
+from restless.endpointParser import parseEndpoints
 
 from models import sqlite, User
 
@@ -20,42 +24,40 @@ class restlessApiPlug:
         if self.application == None:
             self.application = Flask(__name__)
 
-        absoluteApplicationPath: str = "sqlite:///"+os.path.join(applicationPath, "db."+sqliteDatabaseName+".sqlite")
-        absoluteEndpointsPath: str = os.path.join(applicationPath, endpointsConfigPath)
+        dataDirectory: str = os.path.join(applicationPath, "data")
+        if not os.path.exists(dataDirectory):
+            os.makedirs(dataDirectory)
 
-        self.application.config["SQLALCHEMY_DATABASE_URI"] = absoluteApplicationPath
+        self.absoluteApplicationPath: str = "sqlite:///"+os.path.join(dataDirectory, "db."+sqliteDatabaseName+".sqlite")
+        self.absoluteEndpointsPath: str = os.path.join(applicationPath, endpointsConfigPath)
+
+        self.application.config["SQLALCHEMY_DATABASE_URI"] = self.absoluteApplicationPath
         self.application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
         sqlite.init_app(self.application)
         with self.application.app_context():
             sqlite.create_all()
         self.manager: Api = Api(self.application)
-    def addResource(self,
+    def loadResource(self,
                     action: Callable, 
                     endpoint: str) -> None:
         if (self.manager == None or action == None or endpoint == None): return
         self.manager.add_resource(action, endpoint)
+    def dynamicLoadResources(self) -> None:
+        endpoints: dict = parseEndpoints(self.absoluteEndpointsPath)
+        for endpointUrl, endpointAction in endpoints.items():
+            if endpointAction == None: continue
+            self.addResource(endpointAction, endpointUrl)
     def start(self,
               shouldShowDebugInformation = True) -> None:
         if (self.application == None): return
         self.application.run(debug=shouldShowDebugInformation)
 
-class GetWeightSensorById(Resource):
-    def get(self):
-        return { "Message" : "Hello World!" }
-
-class IsWeightSensorOccupiedById(Resource):
-    def get(self):
-        return { "Message" : "Hello World Again!" }
-
 def main() -> None: 
     restlessApi: restlessApiPlug = restlessApiPlug(BASEPATH, 
                                                    "endpoints.json", "persistent",
                                                    True, None)
-    restlessApi.addResource(GetWeightSensorById, 
-                            "/sensor/weight/getById")
-    restlessApi.addResource(IsWeightSensorOccupiedById,
-                            "/sensor/weight/getOccupiedById")
+    restlessApi.dynamicLoadResources()
     restlessApi.start(True)
 
 if __name__ == "__main__":
