8b418fa5df0ca9f52797f30faf83bef591097580
Kasper Anon <kasperthornsten@gmail.com>
2025-02-01 17:59:58 +0100

    Implement basic string to class functionality for use in dynamic dispatch

--- smart-fridge/src/utils.py
+++ smart-fridge/src/utils.py
@@ -1,2 +1,6 @@
+import importlib
 
-
+def stringToClassUsingModule(classNameAsString: str, module: str) -> object:
+    if (classNameAsString == None or module == None): return None
+    using: object = importlib.import_module(module)
+    return getattr(using, classNameAsString)
