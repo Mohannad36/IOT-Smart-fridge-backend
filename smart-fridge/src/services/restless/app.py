47078230e77d15a245acfbe13be19d5133f9af90
Kasper Anon <kasperthornsten@gmail.com>
2025-02-02 22:14:06 +0100

    Restructure restless api to work with new workspace system

--- smart-fridge/src/services/restless/app.py
+++ smart-fridge/src/services/restless/app.py
@@ -1,37 +1,41 @@
 import os
 import json
 
+from workspace import workspace
+
 import importlib
 
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api, Resource
 
-from endpointParser import parseEndpoints
+from modules.endpointParser import parseEndpoints
 
 from database.models import sqlite, User
 
 from collections.abc import Callable
 
-BASEPATH: str = os.path.abspath(os.path.dirname(__file__))
+workspace["RestlessBasePath"] = os.path.abspath(os.path.dirname(__file__))
+workspace["RestlessRoutePath"] = os.path.abspath(os.path.join(workspace["RestlessBasePath"], "routing"))
 
 class restlessApiPlug:
     def __init__(self,
-                 applicationPath: str,
-                 endpointsConfigPath: str, sqliteDatabaseName: str,
+                 databasePath: str,
+                 endpointsConfigPath: str, 
+                 sqliteDatabaseName: str,
                  newApplication: bool = True, application: Flask = None) -> None:
         self.application: Flask = application
         if self.application == None:
             self.application = Flask(__name__)
 
-        dataDirectory: str = os.path.join(applicationPath, "data")
-        if not os.path.exists(dataDirectory):
-            os.makedirs(dataDirectory)
+        workspace["RestlessDatabasePath"] = os.path.abspath(os.path.join(os.path.join(workspace["RestlessBasePath"], databasePath), "data"))
+        workspace["RestlessDatabaseFilePath"] = "sqlite:///"+os.path.abspath(os.path.join(workspace["RestlessDatabasePath"], "db."+sqliteDatabaseName+".sqlite"))
+        workspace["RestlessEndpointsConfigPath"] = os.path.abspath(os.path.join(workspace["RestlessBasePath"], endpointsConfigPath))
 
-        self.absoluteApplicationPath: str = "sqlite:///"+os.path.join(dataDirectory, "db."+sqliteDatabaseName+".sqlite")
-        self.absoluteEndpointsPath: str = os.path.join(applicationPath, endpointsConfigPath)
+        if not os.path.exists(workspace["RestlessDatabasePath"]):
+            os.makedirs(workspace["RestlessDatabasePath"])
 
-        self.application.config["SQLALCHEMY_DATABASE_URI"] = self.absoluteApplicationPath
+        self.application.config["SQLALCHEMY_DATABASE_URI"] = workspace["RestlessDatabaseFilePath"]
         self.application.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
         sqlite.init_app(self.application)
@@ -44,7 +48,7 @@ class restlessApiPlug:
         if (self.manager == None or action == None or endpoint == None): return
         self.manager.add_resource(action, endpoint)
     def dynamicLoadResources(self) -> None:
-        endpoints: dict = parseEndpoints(self.absoluteEndpointsPath)
+        endpoints: dict = parseEndpoints(workspace["RestlessEndpointsConfigPath"])
         for endpointUrl, endpointAction in endpoints.items():
             if endpointAction == None: continue
             self.loadResource(endpointAction, endpointUrl)
@@ -54,8 +58,9 @@ class restlessApiPlug:
         self.application.run(debug=shouldShowDebugInformation)
 
 def main() -> None: 
-    restlessApi: restlessApiPlug = restlessApiPlug(BASEPATH, 
-                                                   "routing/endpoints.json", "persistent",
+    restlessApi: restlessApiPlug = restlessApiPlug(".",
+                                                   "routing/endpoints.json", 
+                                                   "persistent",
                                                    True, None)
     restlessApi.dynamicLoadResources()
     restlessApi.start(True)
