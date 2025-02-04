b669a2fb577479104eb8307446fa7890722b7a2d
ghosthookcc <kasperthornsten@gmail.com>
2025-02-04 17:14:41 +0100

    Restructure receiver.py into its own service called receiver

---
+++ smart-fridge/src/services/receiver/sample_sender.py
@@ -0,0 +1,11 @@
+import socket
+
+def main() -> None:
+    clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    clientSocket.connect(("0.0.0.0", 12444))
+
+    for i in range(10):
+        clientSocket.send("<SENSOR=movement1;TYPE=sensor;>Hello from esp32!<END>".encode())
+
+if __name__ == "__main__":
+    main()
