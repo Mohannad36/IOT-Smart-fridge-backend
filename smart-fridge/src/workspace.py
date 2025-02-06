03e67818f3bc5d6d79b5064dcab46717c8ae9cf9
ghosthookcc <kasperthornsten@gmail.com>
2025-02-06 22:27:10 +0100

    Change config system to use static methods

--- smart-fridge/src/workspace.py
+++ smart-fridge/src/workspace.py
@@ -1,21 +1,47 @@
 import os
+import json
 
-class BaseWorkspace:
+from jsonmerge import merge
+
+class Workspace:
+    config = { "BasePath" : os.path.dirname(os.path.abspath(__file__)) }
     def __getitem__(self, indice):
-        return config[indice]
+        pass
     def __setitem__(self, indice, data):
-        config[indice] = data;
+        pass
+    def __str__(self):
+        stringRepresentation = ""
+        for key, value in self.config.items():
+            stringRepresentation += "(" + key + "::" + value + ") "
+        return stringRepresentation
 
-global config
-config = {
-        "BasePath" : os.path.dirname(__file__),
-}
+def getConfigAttribute(attribute):
+    if not os.path.exists(".env"):
+        newEnvironmentFile = open(".env", "w+")
+        newEnvironmentFile.close()
+    if os.path.getsize(".env") > 0:
+        with open (".env", "r") as paths:
+            Workspace.config = json.load(paths)
+    return Workspace.config[attribute]
 
-global workspace
-workspace = BaseWorkspace()
+def setConfigAttribute(attribute, value):
+    if not os.path.exists(".env"):
+        newEnvironmentFile = open(".env", "w+")
+        newEnvironmentFile.close()
+
+    Workspace.config[attribute] = value
+    currentJsonConfig: str = None
+    newPathsAsJson: str = json.dumps(Workspace.config)
+
+    if os.path.getsize(".env") > 0:
+        with open (".env", "r") as paths:
+            currentJsonConfig = json.load(paths)
+    with open (".env", "w+") as paths:
+        paths.write(merge(currentJsonConfig, newPathsAsJson))
 
 def main() -> None:
-    pass
+    if not os.path.exists(".env"):
+        newEnvironmentFile = open(".env", "w+") 
 
 if __name__ == "__main__":
     main()
