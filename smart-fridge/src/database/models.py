73ab00cff4a4f80f4526a0c5273e79d686d8231f
Kasper Anon <kasperthornsten@gmail.com>
2025-02-12 14:21:46 +0100

    Refactor models.py to match sql file

--- smart-fridge/src/database/models.py
+++ smart-fridge/src/database/models.py
@@ -1,4 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import CheckConstraint
+from sqlalchemy.dialects.mysql import INTEGER
 from datetime import datetime
 
 db = SQLAlchemy()
@@ -7,28 +9,31 @@ class Users(db.Model):
     __tablename__ = 'Users'
     user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     username = db.Column(db.String, nullable=False)
-    pincode = db.Column(db.String, nullable=False)
-    preferences = db.Column(db.String)
+    pincode = db.Column(db.Double, nullable=False)
 
 class Fridges(db.Model):
     __tablename__ = 'Fridges'
     fridge_guid = db.Column(db.String, primary_key=True)
-    model = db.Column(db.JSON)
+    model = db.Column(db.JSON, nullable=False)
 
 class Connections(db.Model):
     __tablename__ = 'Connections'
     connection_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    fridge_guid = db.Column(db.String, db.ForeignKey('Fridges.fridge_guid'))
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'))
+    fridge_guid = db.Column(db.String, db.ForeignKey('Fridges.fridge_guid'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'), nullable=False)
 
 class Items(db.Model):
     __tablename__ = 'Items'
     item_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    quantity = db.Column(db.Float)
+    quantity = db.Column(INTEGER(unsigned=True))
     expiration_date = db.Column(db.Date)
-    fridge_guid = db.Column(db.Integer, db.ForeignKey('Fridges.fridge_guid'))
-    #user_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'))
+
+    fridge_guid = db.Column(db.String, db.ForeignKey('Fridges.fridge_guid'), nullable=False)
+    list_id = db.Column(db.Integer, db.ForeignKey('ShoppingLists.list_id'), nullable=False)
+    user_id = db.Column(db.Integer)
+
+    CheckConstraint("quantity > 0", name="Check for minimum quantity")
 
 class Sensors(db.Model):
     __tablename__ = 'Sensors'
@@ -43,11 +48,6 @@ class ShoppingLists(db.Model):
     list_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
     created_date = db.Column(db.Date)
-    user_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'))
 
-#class ListItems(db.Model):
-#    __tablename__ = 'ListItems'
-#    list_item_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-#    list_id = db.Column(db.Integer, db.ForeignKey('ShoppingLists.list_id'))
-#    item_id = db.Column(db.Integer, db.ForeignKey('Items.item_id'))
-#    quantity = db.Column(db.Float)
+    fridge_guid = db.Column(db.String, db.ForeignKey('Items.fridge_guid'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'), nullable=False)
