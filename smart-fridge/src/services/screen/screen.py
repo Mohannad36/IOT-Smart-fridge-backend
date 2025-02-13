3d2862038141580dbfbe44beedd9b688c3891a0a
ghosthookcc <kasperthornsten@gmail.com>
2025-02-13 16:27:38 +0100

    Implement logging for screen-service

--- smart-fridge/src/services/screen/screen.py
+++ smart-fridge/src/services/screen/screen.py
@@ -1,14 +1,19 @@
 import time
 
+from modules.logging import logger
+
+log: logger = logger("screen-service.log", "INFO")
+
 class screenPlug:
     def __init__(self) -> any:
         pass
 
 def main() -> None:
+    log.info("Started")
     screen = screenPlug()
     while True:
         time.sleep(1)
-
+    log.info("Finished")
 
 if __name__ == "__main__":
     main()
