b9c7fed007085e595f8a384bc0f56aca163a30ca
Kasper Anon <kasperthornsten@gmail.com>
2025-02-13 23:02:10 +0100

    Implement reverse proxy for handling messages sent from websockets to server

---
+++ smart-fridge/src/services/websocket-proxy/proxy.py
@@ -0,0 +1,58 @@
+import json
+import asyncio
+import socket
+import sqlalchemy
+
+from workspace import workspace
+
+from websockets.asyncio.server import serve
+
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from sqlalchemy import func
+
+from database.models import Users
+
+from functools import partial
+
+sqlEngine = sqlalchemy.create_engine(workspace.getConfigAttribute("RestlessDatabaseFilePath"))
+
+CONNECTIONS = {}
+
+async def sendToReverseProxy(message, reader, writer):
+    writer.write(message.encode())
+    await writer.drain()
+
+    data = await reader.read(1024)
+
+    return data.decode()
+
+async def proxyServer(websocket, reader, writer):
+    async for message in websocket:
+        event = json.loads(message)
+
+        userFound: bool = False
+        stmt = select(func.count()).select_from(Users).where(Users.username == event["username"]).where(Users.pincode  == event["pincode"])
+        with Session(sqlEngine) as session:
+            userFound = True if session.execute(stmt).scalar_one() > 0 else False
+
+        if not userFound:
+            await websocket.close()
+            return
+
+        response = await sendToReverseProxy(event["data"], reader, writer)
+        print(f"Data received :: {event['data']} . . .")
+
+        if len(response) > 0:
+            await websocket.send(response)
+
+async def main() -> None:
+    proxyReader, proxyWriter = await asyncio.open_connection("192.168.1.92", 12444)
+    async with serve(partial(proxyServer, reader=proxyReader, writer=proxyWriter), "localhost", 13000):
+        await asyncio.get_running_loop().create_future()
+
+def runMain():
+    asyncio.run(main())
+
+if __name__ == "__main__":
+    runMain()
