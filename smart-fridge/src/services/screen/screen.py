d3b07124612df52dd785be9bbb7131316c98c9cd
Kasper Anon <kasperthornsten@gmail.com>
2025-02-13 01:35:41 +0100

    Made screen.py run forever with a delayed while loop

--- smart-fridge/src/services/screen/screen.py
+++ smart-fridge/src/services/screen/screen.py
@@ -1,9 +1,14 @@
+import time
+
 class screenPlug:
     def __init__(self) -> any:
         pass
 
 def main() -> None:
     screen = screenPlug()
+    while True:
+        time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
