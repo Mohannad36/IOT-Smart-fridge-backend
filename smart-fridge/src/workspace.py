2e1247fc983e43773a33ce9bd620194d94657dc1
Kasper Anon <kasperthornsten@gmail.com>
2025-02-13 01:38:13 +0100

    Implement workspace.py as the main application

--- smart-fridge/src/workspace.py
+++ smart-fridge/src/workspace.py
@@ -1,7 +1,12 @@
 import os
 import json
+import psutil
+import signal
 
 from jsonmerge import merge
+
+from modules.utils import guid
+from modules.cache import *
 
 class Workspace:
     config = { "BasePath" : os.path.dirname(os.path.abspath(__file__)) }
@@ -42,6 +47,52 @@ def setConfigAttribute(attribute, value):
 def main() -> None:
     if not os.path.exists(".env"):
         newEnvironmentFile = open(".env", "w+") 
+    setConfigAttribute("GUID", guid())
+    setupCaching()
+
+    restlessServiceProcess: any = None
+    receiverServiceProcess: any = None
+    screenServiceProcess: any = None
+
+    savedServicePidLines: list = []
+
+    if os.path.getsize(getConfigAttribute("cacheFilePath")) > 0:
+        with open(getConfigAttribute("cacheFilePath"), "r") as cache:
+            for servicePidLine in cache:
+                if len(servicePidLine) == 0 or servicePidLine == "\n": 
+                    continue
+
+                serviceName, pid = servicePidLine.split(":")
+                pid = int(pid)
+
+                if psutil.pid_exists(pid):
+                    match serviceName:
+                        case "restless":
+                            restlessServiceProcess = psutil.Process(pid)
+                            print("NO START RESTLESS")
+                        case "receiver":
+                            receiverServiceProcess = psutil.Process(pid)
+                            print("NO START RECEIVER")
+                        case "screen":
+                            screenServiceProcess = psutil.Process(pid)
+                            print("NO START SCREEN")
+                    savedServicePidLines.append(servicePidLine)
+    with open(getConfigAttribute("cacheFilePath"), "w+") as cache:
+        if not restlessServiceProcess:
+            restlessServiceProcess = runSubprocessControlledCached(["poetry", "run", "restless-service"])
+            print("START RESTLESS")
+            cache.write(f"restless:{restlessServiceProcess.pid}\n")
+        if not receiverServiceProcess:
+            receiverServiceProcess = runSubprocessControlledCached(["poetry", "run", "receiver-service"])
+            print("START RECEIVER")
+            cache.write(f"receiver:{receiverServiceProcess.pid}\n")
+        if not screenServiceProcess:
+            screenServiceProcess = runSubprocessControlledCached(["poetry", "run", "screen-service"])
+            print("START SCREEN")
+            cache.write(f"screen:{screenServiceProcess.pid}\n")
+        for savedServicePidLine in savedServicePidLines:
+            cache.write(savedServicePidLine+"\n")
+
 
 if __name__ == "__main__":
     main()
