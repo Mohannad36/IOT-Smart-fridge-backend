fc7c9a51c24bc52c3ff809daaa7095b7c929f439
Kasper Anon <kasperthornsten@gmail.com>
2025-02-13 23:22:56 +0100

    Change string content to be more descriptive

--- smart-fridge/src/services/websocket-proxy/proxy.py
+++ smart-fridge/src/services/websocket-proxy/proxy.py
@@ -55,7 +55,7 @@ async def proxyServer(websocket, reader, writer):
 
 async def main() -> None:
     log.info("Started")
-    proxyReader, proxyWriter = await asyncio.open_connection("192.168.1.92", 12444)
+    proxyReader, proxyWriter = await asyncio.open_connection("<YOUR_IP>", 12444)
     async with serve(partial(proxyServer, reader=proxyReader, writer=proxyWriter), "localhost", 13000):
         await asyncio.get_running_loop().create_future()
     log.info("Finished")
