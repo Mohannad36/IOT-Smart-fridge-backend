e63b9f27b7761ab2d69ae060e7a0470a577aa4c5
ghosthookcc <kasperthornsten@gmail.com>
2025-02-18 13:03:17 +0100

    Implement swagger documentation route as returned JSON

--- smart-fridge/src/services/restless/routing/routeActions.py
+++ smart-fridge/src/services/restless/routing/routeActions.py
@@ -1,3 +1,4 @@
+from flask import jsonify
 from flask_restful import Resource
 
 class GetWeightSensorById(Resource):
@@ -7,3 +8,13 @@ class GetWeightSensorById(Resource):
 class IsWeightSensorOccupiedById(Resource):
     def get(self):
         return { "Message" : "Hello World Again!" }
+
+class SwaggerJson(Resource):
+    def get(self):
+        return jsonify({
+            "swagger" : "2.0",
+            "info" : {
+                "title" : "Restless smart-fridge API",
+                "version" : "1.0",
+                "description" : "Swagger smart-fridge API documentation"
+            }})
